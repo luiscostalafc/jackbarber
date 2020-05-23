2bb9362f50f9d2ed94dec641625e24a3b9ff8e35
luiscostalafc <luiscostalafc@gmail.com>
2020-05-23 13:20:38 -0300

    Refatorando Migrations

--- mobile/src/config/ReactotronConfig.js
+++ mobile/src/config/ReactotronConfig.js
@@ -6,7 +6,7 @@ import AsyncStorage from '@react-native-community/async-storage';
 
 if (__DEV__) {
 	const tron = Reactotron.setAsyncStorageHandler(AsyncStorage)
-		.configure({ host: '192.168.0.6' })
+		.configure({ host: '167.71.86.178' })
 		.useReactNative()
 		.use(reactotronRedux())
 		.use(sagaPlugin())
